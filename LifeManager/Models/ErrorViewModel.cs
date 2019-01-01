cae063c90e5f620efc9fcee689ec5ec7cf0041cb
D'va <D'va@DESKTOP-KUO1M9C>
2019-01-01 13:25:57 +0300

    Very beginning

---
+++ LifeManager/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace LifeManager.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
